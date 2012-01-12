834150b3cfaf28f07d6a8fdc99442daa583d3201
Ioan Rogers <ioan.rogers@gmail.com>
2012-01-12 05:31:11 -0800

    tidied, check email isn't in the author_name

--- lib/Dist/Zilla/Plugin/ChangelogFromGit/CPAN/Changes.pm
+++ lib/Dist/Zilla/Plugin/ChangelogFromGit/CPAN/Changes.pm
@@ -7,52 +7,52 @@ use CPAN::Changes;
 use CPAN::Changes::Release;
 
 extends 'Dist::Zilla::Plugin::ChangelogFromGit';
- 
+
 sub render_changelog {
     my ($self) = @_;
- 
-    my $cpan_changes = CPAN::Changes->new(
-        preamble => 'Changelog for ' . $self->zilla->name,
-    );
-     
-    foreach my $release (reverse $self->all_releases) {
-        next if $release->has_no_changes; # no empties
- 
+
+    my $cpan_changes = CPAN::Changes->new( preamble => 'Changelog for ' . $self->zilla->name, );
+
+    foreach my $release ( reverse $self->all_releases ) {
+        next if $release->has_no_changes;    # no empties
+
         my $version = $release->version;
-        if ($version eq 'HEAD') {
+        if ( $version eq 'HEAD' ) {
             $version = $self->zilla->version;
         }
-                
+
         my $cpan_release = CPAN::Changes::Release->new(
             version => $version,
             date    => $release->date,
         );
-         
-        foreach my $change (@{ $release->changes }) {             
-            next if ($change->description =~ /^\s/); # no empties
-            my $committer_str = $change->author_name;
 
- #           if ($change->author_email) {
-#                $committer_str .= ' <' . $change->author_email . '>';
-  #          }
-            
-            $cpan_release->add_changes(
-                { group => $committer_str },
-                $change->description,
-            );
+        foreach my $change ( @{ $release->changes } ) {
+            next if ( $change->description =~ /^\s/ );    # no empties
+            my $group = $change->author_name;
+
+            # sometimes author_name contains the "Full Name <email@address.com>"
+            # sometimes not, so do a lazy check
+            if ( $change->author_name !~ m/@/ ) {
+                $group .= ' <' . $change->author_email . '>';
+            }
+
+            # XXX: do we want the change_id?
+            # $group .= ' ' .  $change->change_id;
+
+            $cpan_release->add_changes( { group => $group }, $change->description, );
         }
 
         $cpan_changes->add_release($cpan_release);
     }
-    
+
     return $cpan_changes->serialize;
 }
- 
- 
+
 __PACKAGE__->meta->make_immutable;
 
 1;
 
 =head1 SEE ALSO
+
 L<Dist::Zilla::Plugin::ChangelogFromGit::Debian> which was used as a template for this
 
